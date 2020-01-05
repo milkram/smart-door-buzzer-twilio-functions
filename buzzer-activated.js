b244a9902922a74664567ced46613cbdef9d9219
Mark Lim <hello@milkram.com>
2020-01-05 22:48:33 +1100

    clean

--- buzzer-activated.js
+++ buzzer-activated.js
@@ -1,25 +1,25 @@
 /**
- *  Simple call box routine
- * 
- *  This function is meant for the apartment building callbox
- *  It gives the user a couple of seconds to produce the password
- * 	Then dials all the residents to grant manual entry
- */
+*  Simple call box routine
+*
+*  This function is meant for the apartment building callbox
+*  It gives the user a couple of seconds to produce the password
+* 	Then dials all the residents to grant manual entry
+*/
+
 exports.handler = function(context, event, callback) {
-
-  let twiml = new Twilio.twiml.VoiceResponse();
-
-  // Gather both speech and digit entry from user
-  twiml.gather({
+  const OPTIONS = {
     action: '/door-open',
     hints: context.PASSPHRASE,
     input: 'speech dtmf',
     numDigits: '4',
     speechTimeout: 'auto',
-    timeout: 2,
-  })
-    .say({voice: 'woman'}, 'Please wait')  
+    timeout: 4,
+  };
+  const GENDER_VOICE = { voice: 'woman '};
+  const PHRASE = 'Please wait';
 
+  const twiml = new Twilio.twiml.VoiceResponse();
+  twiml.gather(OPTIONS).say(GENDER_VOICE, PHRASE); // gather speech+digit entry from user
   twiml.redirect('/call-residents')
-  callback(null, twiml)  
+  callback(null, twiml)
 }
